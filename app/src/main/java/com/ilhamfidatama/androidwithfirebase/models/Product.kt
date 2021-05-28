55f73dabde10ecc4768c5b88e34c23b2ea9dd4c7
ilhamfidatama <ilhamfidatama@gmail.com>
2021-05-29 00:23:07 +0800

    product and store models

---
+++ app/src/main/java/com/ilhamfidatama/androidwithfirebase/models/Product.kt
@@ -0,0 +1,8 @@
+package com.ilhamfidatama.androidwithfirebase.models
+
+data class Product(
+    var name: String,
+    var price: Double,
+    var disc: Double,
+    var discPrice: Double
+)
---
+++ app/src/main/java/com/ilhamfidatama/androidwithfirebase/models/Store.kt
@@ -0,0 +1,7 @@
+package com.ilhamfidatama.androidwithfirebase.models
+
+data class Store(
+    var name: String,
+    var address: String,
+    var products: MutableList<Product> = mutableListOf()
+)
