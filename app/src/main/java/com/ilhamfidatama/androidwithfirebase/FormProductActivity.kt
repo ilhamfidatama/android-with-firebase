5c285a170865438b56278591282a328d541f6ea0
ilhamfidatama <ilhamfidatama@gmail.com>
2021-05-28 22:02:07 +0800

    form product activity

---
+++ app/src/main/java/com/ilhamfidatama/androidwithfirebase/FormProductActivity.kt
@@ -0,0 +1,11 @@
+package com.ilhamfidatama.androidwithfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_product)
+    }
+}
